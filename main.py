6fecb5ef791f371e258011a0918bee09316016a1
LukasJuen <93828796+LukasJuen@users.noreply.github.com>
2023-01-20 13:21:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@
 #main
 
 
+import subprocess
+
+script1 = "/home/Pi/Desktop/Test/Temp_cam.py"
+script2 = "/home/Pi/Desktop/Test/PiLogger/PiLogger.py"
+
+subprocess.Popen(["python", script1])
+subprocess.Popen(["python", script2])
+
+
