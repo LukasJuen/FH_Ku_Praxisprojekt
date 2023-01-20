2b5ecea507098a02145563432ade079d39a29649
LukasJuen <93828796+LukasJuen@users.noreply.github.com>
2023-01-20 14:00:51 +0100

    Update Temp__cam.py

--- Temp__cam.py
+++ Temp__cam.py
@@ -27,9 +27,9 @@ import numpy as np
 import digitalio
 
 # Define input
-input_1 = digitalio.DigitalInOut(board.D16)
-input_1.direction = digitalio.Direction.INPUT
-input_1.pull = digitalio.Pull.UP
+#input_1 = digitalio.DigitalInOut(board.D16)
+#input_1.direction = digitalio.Direction.INPUT
+#input_1.pull = digitalio.Pull.UP
 
 
 # Create an empty list to store images
