4d43d1bbb2b100f0ee2fb267b9b72cf13e7035f9
LukasJuen <93828796+LukasJuen@users.noreply.github.com>
2023-01-20 13:22:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 
 import subprocess
 
-script1 = "/home/Pi/Desktop/Test/Temp_cam.py"
-script2 = "/home/Pi/Desktop/Test/PiLogger/PiLogger.py"
+script1 = "/home/Pi/Desktop/Test/FH_Ku_Praxisprojekt/Temp_cam.py"
+script2 = "/home/Pi/Desktop/Test/FH_Ku_Praxisprojekt/PiLogger/PiLogger.py"
 
 subprocess.Popen(["python", script1])
 subprocess.Popen(["python", script2])
